eea0f735523c89fb5a11edaeaedce03c31e15017
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-09-14 10:19:29 -0700

    first commit
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

---
+++ src/cai/session_based_builder.py
@@ -0,0 +1,78 @@
+from ray.serve.llm import LLMConfig, LLMRouter, LLMServer
+from ray.serve.config import RequestRouterConfig
+from ray.serve.context import _get_internal_replica_context
+from ray.serve.deployment import Application
+from ray.serve._private.common import ReplicaID
+
+from cai.session_router import SessionAwareRequestRouter
+
+
+from typing import Dict, Any
+
+class SesssionAwareMixin:
+    
+    async def __init__(self):
+        
+        context = _get_internal_replica_context()
+        self.replica_id: ReplicaID = context.replica_id
+        
+        # TODO: Make this a Capped set of size 1000 or sth that has LRU eviction
+        self.hot_sessions = set()
+        
+    def _parse_session_id(self, request):
+        # Extra args from request
+        xargs = getattr(request, "vllm_xargs", {})
+        return xargs.get("session_id")
+    
+    def record_routing_stats(self) -> Dict[str, Any]:
+        return {
+            "hot_sessions": self.hot_sessions
+        }
+
+class SessionAwareLLMServer(LLMServer, SesssionAwareMixin):
+        
+    async def _run_request(
+        self,
+        request,
+        *,
+        engine_method: str,
+        batch_output_stream: bool = False,
+    ):
+        # from session aware mixin
+        session_id = self._parse_session_id(request)
+        if session_id:
+            self.hot_sessions.add(session_id)
+        
+        return await super()._run_request(
+            request,
+            engine_method=engine_method,
+            batch_output_stream=batch_output_stream,
+        )
+
+def build(serving_config_dict: Dict[str, Any]) -> Application:
+    
+    llm_configs = serving_config_dict["llm_configs"]
+    
+    if len(llm_configs) != 1:
+        raise ValueError("Only one LLM config is supported")
+    
+    
+    llm_config = LLMConfig(**llm_configs[0])
+    # TODO: Builder of LLM Deployment is not generic enough for custom LLMServers. 
+    # llm_deployment = build_llm_deployment(llm_config)
+    deployment_options = llm_config.get_serve_options(
+        name_prefix="LLMServer:",
+    )
+    
+    # Update the deployment option with the session aware request router
+    deployment_options["request_router_config"] = RequestRouterConfig(
+        request_router_class=SessionAwareRequestRouter,
+        request_routing_stats_period_s=2,
+        request_routing_stats_timeout_s=1,
+    )
+
+    llm_deployment = SessionAwareLLMServer.as_deployment(
+        deployment_options).bind(llm_config=llm_config)    
+    app = LLMRouter.as_deployment(llm_configs=[llm_config]).bind(
+        llm_deployments=[llm_deployment])
+    return app
---
+++ src/cai/session_router.py
@@ -0,0 +1,61 @@
+
+
+from ray.serve._private.request_router.common import PendingRequest
+from ray.serve._private.request_router.replica_wrapper import RunningReplica
+from ray.serve._private.request_router.pow_2_router import PowerOfTwoChoicesRequestRouter
+
+from typing import List, Optional
+
+class SessionAwareRequestRouter(PowerOfTwoChoicesRequestRouter):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(f"[DEBUG] SessionAwareRequestRouter initialized.")
+
+
+    def _extract_session_id(self, request: PendingRequest):
+        for arg in request.args:
+            xargs = None
+            if isinstance(arg, dict):
+                xargs = arg.get("vllm_xargs", {})
+            else:
+                xargs = getattr(arg, "vllm_xargs", {})
+            if xargs: 
+                return xargs.get("session_id")
+    
+    
+    def _find_matched_replica(self, candidate_replicas: List[RunningReplica], pending_request: Optional[PendingRequest]) -> Optional[RunningReplica]:
+        """
+        Find the replica that matches the session-id in the request.
+        """
+        if not pending_request:
+            return 
+        
+        session_id = self._extract_session_id(pending_request)
+        if session_id:
+            for replica in candidate_replicas:
+                routing_stats = replica.routing_stats
+                if not routing_stats:
+                    continue
+                
+                hot_sessions = routing_stats.get('hot_sessions', set())
+                if session_id in hot_sessions:
+                    return replica
+        return None
+    
+    
+    async def choose_replicas(
+        self,
+        candidate_replicas: List[RunningReplica],
+        pending_request: Optional[PendingRequest] = None,
+    ) -> List[List[RunningReplica]]:
+
+        # Update our session mapping from latest replica stats
+        matched_replica = self._find_matched_replica(candidate_replicas, pending_request)
+        
+        if matched_replica:
+            return [[matched_replica]]
+
+        # Fallback to PowerOfTwoChoicesRequestRouter
+        return await super().choose_replicas(candidate_replicas, pending_request)
+    
